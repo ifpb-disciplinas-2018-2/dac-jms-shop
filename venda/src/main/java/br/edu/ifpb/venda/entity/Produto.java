f2d2f97a507ff3706b61061d1b659c23a9bdab85
MurilloTavares <murillo_eum@hotmail.com>
2019-03-14 10:02:50 -0300

    Produto

---
+++ venda/src/main/java/br/edu/ifpb/venda/entity/Produto.java
@@ -0,0 +1,46 @@
+package br.edu.ifpb.venda.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String descricao;
+    private float valor;
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+}
