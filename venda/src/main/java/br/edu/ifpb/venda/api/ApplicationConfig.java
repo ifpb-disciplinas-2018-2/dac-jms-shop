7c6c98d0a7a0d8733269927856bb617d5e7a1a91
MurilloTavares <murillo_eum@hotmail.com>
2019-03-21 10:04:51 -0300

    ApplicationConfig

---
+++ venda/src/main/java/br/edu/ifpb/venda/api/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.venda.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author murillo
+ */
+@ApplicationPath("api")
+public class ApplicationConfig extends Application{
+    
+}
